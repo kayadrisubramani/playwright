5371aef7e4c0394d36d2875bd05c9d872185aa3a
kayadrisubramani <playwright786@gmail.com>
2025-08-17 19:42:47 +0530

    Add files via upload

---
+++ primitivedatatype.js
@@ -0,0 +1,37 @@
+var firstname="kayadri"
+const rollnum=123456
+var currentcity="chennai"
+const subject="English"
+let mark=70
+let isWeekend=true
+let futureGoal
+console.log (firstname +  typeof (firstname))
+console.log (rollnum + typeof(rollnum))
+console.log("I reside in" + currentcity + typeof(currentcity))
+console.log(mark+10 , typeof(mark))
+console.log(isWeekend + typeof (isWeekend))
+console.log(futureGoal + typeof (futureGoal))
+
+// Print Marks whether Pass or Fail
+//var mark=90
+//var mark=100
+console.log(mark+10)
+if (mark>80){
+console.log("mark is Pass")
+}
+else{  
+console.log("mark is Fail")
+}
+// Isweekend scenario
+
+if (isWeekend=true){
+console.log("Relax")
+}
+else{  
+console.log("Work")
+}
+
+// ***********Notes about Var****************//
+//Var can be redeclared and reassigned where as let can be reassigned but cannot redeclared
+//Var can be globally declared where as let can be declared only inside the blocks
+ 
