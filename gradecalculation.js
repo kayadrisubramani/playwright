175036e97e1b3b188df6ea0d867fa10797568ee9
kayadrisubramani <playwright786@gmail.com>
2025-08-21 13:02:49 +0530

    Add files via upload

---
+++ gradecalculation.js
@@ -0,0 +1,18 @@
+
+let score =75
+
+function functionName(score){
+switch(true){
+        case(score>90):
+            console.log("Grade is A")
+            break;
+        case(score>70):
+            console.log("Grade is B")
+        break;
+        case(score>50):
+            console.log("Grade is C")     
+}
+}
+
+functionName(score)
+
